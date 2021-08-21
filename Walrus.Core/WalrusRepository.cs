f91dab40a622e4617e72619f56954f0896332ff0
Cory Todd <cory@ardonyx.io>
2021-08-21 14:04:00 -0700

    fix crash commit enumerator
    Some repos seem to crash LibGit2Sharp so a fix has been devised.
    We simply manually iterate using the iterator so we can catch
    the exception and abort safely.

--- Walrus.Core/WalrusRepository.cs
+++ Walrus.Core/WalrusRepository.cs
@@ -1,4 +1,5 @@
 ﻿using LibGit2Sharp;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -37,13 +38,73 @@ namespace Walrus.Core
         /// <returns>List of matching commits</returns>
         public IEnumerable<WalrusCommit> GetCommitsInRange(DateTime start, DateTime end)
         {
-            foreach(var commit in _repository.Commits)
+            var commitIter = _repository.Commits.GetEnumerator();
+            foreach (var commit in SafeGitCommitEnumeration(commitIter, start, end))
             {
-                if(commit.Author.When >= start && commit.Author.When < end)
+                yield return commit;
+            }
+        }
+
+
+        /// <summary>
+        /// Returns list of all commits in this repo on all branches in specified time range
+        /// </summary>
+        /// <param name="start">Starting timestamp, inclusive</param>
+        /// <param name="end">ending timestamp, exclusive</param>
+        /// <returns>List of matching commits</returns>
+        public IEnumerable<WalrusCommit> GetCommitsInRangeAllBranches(DateTime start, DateTime end)
+        {
+            WalrusLog.Logger.LogDebug("Querying commits from all branches, this may take a momeny");
+
+            foreach (var branch in _repository.Branches)
+            {
+                if (branch.IsRemote)
+                {
+                    continue;
+                }
+
+                var commitIter = branch.Commits.GetEnumerator();
+                foreach (var commit in SafeGitCommitEnumeration(commitIter, start, end))
+                {
+                    yield return commit;
+                }
+            }
+        }
+
+        /// <summary>
+        /// LibGit2Sharp Commits iterator seems a bit crashy so we'll manually iterate
+        /// </summary>
+        /// <param name="commitIter">Commit iterator</param>
+        /// <param name="start">Return commits on or after this date</param>
+        /// <param name="end">Return commit before this date</param>
+        /// <returns>Commits that satisfy filter</returns>
+        private IEnumerable<WalrusCommit> SafeGitCommitEnumeration(IEnumerator<Commit> commitIter, DateTime start, DateTime end)
+        {
+            do
+            {
+                try
+                {
+                    // This may crash with an invalid object exception
+                    if (!commitIter.MoveNext())
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    WalrusLog.Logger.LogError(ex, "Error enumerating commits on {Path}", RepositoryPath);
+                    break;
+                }
+
+                var commit = commitIter.Current;
+                if (commit.Author.When >= start && commit.Author.When < end)
                 {
                     yield return new WalrusCommit(commit);
                 }
-            }
+            } while (true);
+
+
+            yield break;
         }
     }
 }
