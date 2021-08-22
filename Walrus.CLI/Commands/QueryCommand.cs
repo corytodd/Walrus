f2e18d204cfb91b0fdc5dd9c880e676634d68db3
Cory Todd <cory@ardonyx.io>
2021-08-21 22:36:38 -0700

    [cli] add table format printer
    For the Query command, add support for pretty-printing a table
    sorted by repo and date.

--- Walrus.CLI/Commands/QueryCommand.cs
+++ Walrus.CLI/Commands/QueryCommand.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.Extensions.Logging;
     using System;
+    using System.Collections.Generic;
     using System.CommandLine;
     using System.CommandLine.Invocation;
     using System.Linq;
@@ -34,16 +35,21 @@
                 ));
 
             Command.AddOption(new Option<string>(
-                "--author-email", 
+                "--author-email",
                 "Return commits from this author"));
 
             Command.AddOption(new Option<string>(
                 "--repo-name",
                 "Return commits from this repository. Case insensitive."));
 
-            Command.Handler = CommandHandler.Create((WalrusQuery query) =>
+            Command.AddOption(new Option(
+                "--print-table",
+                "Print results in a tree format"
+                ));
+
+            Command.Handler = CommandHandler.Create((WalrusQuery query, bool printTable) =>
             {
-                HandleQuery(query);
+                HandleQuery(query, printTable);
             });
         }
 
@@ -57,7 +63,8 @@
         /// Execute Git query
         /// </summary>
         /// <param name="query">Query to execute</param>
-        private void HandleQuery(WalrusQuery query)
+        /// <param name="printTable">True to print results as table</param>
+        private void HandleQuery(WalrusQuery query, bool printTable)
         {
             _logger.LogDebug("HandleQuery: {query}", query);
 
@@ -67,11 +74,46 @@
                 .AsParallel()
                 .Select(r => r.GetCommits(query))
                 .SelectMany(c => c)
-                .OrderBy(c => c.Timestamp);
+                .OrderBy(c => c.Timestamp)
+                .AsEnumerable();
+
+            if (printTable)
+            {
+                PrintTable(commits);
+            }
+        }
+
+        /// <summary>
+        ///     Rough table format printer for commit list
+        ///     Commits are sorted by repo then by day, a summary 
+        ///     SHA and commit title are shown for each commit.
+        /// </summary>
+        /// <param name="commits">Commits to print</param>
+        private void PrintTable(IEnumerable<WalrusCommit> commits)
+        {
+            var header = new string('-', Console.WindowWidth / 2);
+
+            foreach (var groupRepo in commits.GroupBy(c => c.RepoName))
+            {
+                Console.WriteLine($"Repository: {groupRepo.Key}");
+                Console.WriteLine(header);
+
+                foreach (var groupDay in groupRepo.GroupBy(g => g.Timestamp.Date))
+                {
+                    Console.WriteLine($"{groupDay.Key:d}: {groupDay.Count()} Commits");
+
+                    foreach(var commit in groupDay)
+                    {
+                        Console.WriteLine($"\t{commit.Sha} {commit.Message}");
+                    }
+                }
+                Console.WriteLine();
+            }
 
             var count = commits.Count();
 
-            Console.WriteLine("Found {0} commits", count);
+            Console.WriteLine(new string('=', Console.WindowWidth / 2));
+            Console.WriteLine("Total Commits: {0}", count);
         }
     }
 }
