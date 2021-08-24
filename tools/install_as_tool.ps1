07d1a8d50b7f8585214275ef3c0c705ea4321b56
Cory Todd <cory@ardonyx.io>
2021-08-23 17:22:47 -0700

    [tools] use release config for tool installation
    Update sample tool install script to use Release build

--- tools/install_as_tool.ps1
+++ tools/install_as_tool.ps1
@@ -1,5 +1,5 @@
 # https://docs.microsoft.com/en-us/dotnet/core/tools/global-tools-how-to-use
 rm ./dist/*.nupkg
-dotnet pack Walrus.CLI
+dotnet pack Walrus.CLI --configuration Release
 dotnet tool uninstall --global corytodd.us.walrusc
 dotnet tool install --global --add-source ./dist corytodd.us.walrusc
